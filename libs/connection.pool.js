4e1abff4a5a9e60627bd374be3a2b4292caa32b0
Tadas Plančiūnas <tadas.planciunas@ferratum.com>
2016-12-28 16:08:35 +0200

    From Ignas: cs fix, fixed ws client matching

--- libs/connection.pool.js
+++ libs/connection.pool.js
@@ -6,144 +6,145 @@
  */
 (function () {
 
+  /**
+   *
+   * @returns {{add: add, remove: remove, filter: (function(Array, (string|Object|function()), (function(actual,
+   *   expected)|true|undefined)))}}
+   * @constructor
+   */
+  function ConnectionPool () {
+
+    var _clients = [];
+
+    return {
+      add: add,
+      get: get,
+      getAll: getAll,
+      remove: remove,
+      filter: filter,
+      update: update,
+      getShelterClients: getShelterClients
+    };
+
     /**
      *
-     * @returns {{add: add, remove: remove, filter: (function(Array, (string|Object|function()), (function(actual, expected)|true|undefined)))}}
-     * @constructor
+     * @param client
      */
-    function ConnectionPool() {
-
-        var _clients = [];
-
-        return {
-            add: add,
-            get: get,
-            getAll: getAll,
-            remove: remove,
-            filter: filter,
-            update: update,
-            getShelterClients: getShelterClients
-        };
-
-        /**
-         *
-         * @param client
-         */
-        function add(client) {
-            _clients.push(client);
-        }
-
-        /**
-         *
-         * @param clientId
-         * @returns {*}
-         */
-        function get(clientId) {
-            var clients = filter({
-                id: clientId
-            });
-
-            if (clients.length === 1) {
-                return clients[0];
-            }
-
-            return false;
-        }
-
-        /**
-         * Get all clients
-         *
-         * @returns {Array}
-         */
-        function getAll() {
-            return _clients;
-        }
-
-        /**
-         * Finds and updates clients
-         *
-         * @param properties
-         * @param update
-         */
-        function update(properties, update) {
-            var matched = filter(properties),
-                index = 0;
-
-            for (index; index < matched.length; index++) {
-                for (var key in update) {
-                    matched[index][key] = update[key];
-                }
-            }
-        }
-
-        /**
-         *
-         * @param properties
-         * @returns {Array}
-         */
-        function filter(properties) {
-            var matched = [],
-                index = 0;
-
-            for (index; index < _clients.length; index++) {
-                var found = 0;
-                for (var key in properties) {
-                    if (_clients[index][key] === properties[key]) {
-                        found++;
-                    }
-                }
-
-                if (found === Object.keys(properties).length) {
-                    matched.push(_clients[index]);
-                }
-            }
-
-            return matched;
-        }
-
-        /**
-         * Returns all connected shelter clients
-         *
-         * @returns {Array}
-         */
-        function getShelterClients() {
-            var matched = [],
-                index = 0;
-
-            for (index; index < _clients.length; index++) {
-                if (_clients[index].id === _clients[index].shelterId) {
-                    matched.push(_clients[index]);
-                }
-            }
-
-            return matched;
-        }
-
-        /**
-         *
-         * @param client
-         */
-        function remove(client) {
-            var index;
-
-            for (index = 0; index < _clients.length; index++) {
-                if (_clients[index] === client) {
-                    _removeByIndex(index);
-                    break;
-                }
-            }
-        }
-
-        /**
-         * Removes a client by index
-         *
-         * @param index
-         * @private
-         */
-        function _removeByIndex(index) {
-            _clients.splice(index, 1);
-        }
+    function add (client) {
+      _clients.push(client);
     }
 
-    module.exports = new ConnectionPool();
+    /**
+     *
+     * @param clientId
+     * @returns {*}
+     */
+    function get (clientId) {
+      var clients = filter({
+        id: clientId
+      });
+
+      if (clients.length === 1) {
+        return clients[0];
+      }
+
+      return false;
+    }
+
+    /**
+     * Get all clients
+     *
+     * @returns {Array}
+     */
+    function getAll () {
+      return _clients;
+    }
+
+    /**
+     * Finds and updates clients
+     *
+     * @param properties
+     * @param update
+     */
+    function update (properties, update) {
+      var matched = filter(properties),
+        index = 0;
+
+      for (index; index < matched.length; index++) {
+        for (var key in update) {
+          matched[index][key] = update[key];
+        }
+      }
+    }
+
+    /**
+     *
+     * @param properties
+     * @returns {Array}
+     */
+    function filter (properties) {
+      var matched = [],
+        index = 0;
+
+      for (index; index < _clients.length; index++) {
+        var found = 0;
+        for (var key in properties) {
+          if (_clients[index][key] == properties[key]) {
+            found++;
+          }
+        }
+
+        if (found === Object.keys(properties).length) {
+          matched.push(_clients[index]);
+        }
+      }
+
+      return matched;
+    }
+
+    /**
+     * Returns all connected shelter clients
+     *
+     * @returns {Array}
+     */
+    function getShelterClients () {
+      var matched = [],
+        index = 0;
+
+      for (index; index < _clients.length; index++) {
+        if (_clients[index].id == _clients[index].shelterId) {
+          matched.push(_clients[index]);
+        }
+      }
+
+      return matched;
+    }
+
+    /**
+     *
+     * @param client
+     */
+    function remove (client) {
+      var index;
+
+      for (index = 0; index < _clients.length; index++) {
+        if (_clients[index] == client) {
+          _removeByIndex(index);
+          break;
+        }
+      }
+    }
+
+    /**
+     * Removes a client by index
+     *
+     * @param index
+     * @private
+     */
+    function _removeByIndex (index) {
+      _clients.splice(index, 1);
+    }
+  }
+
+  module.exports = new ConnectionPool();
 })();
--- libs/message.proxy.js
+++ libs/message.proxy.js
@@ -6,83 +6,83 @@
  */
 
 (function () {
-    function MessageProxy() {
-        var connectionPool = require('./connection.pool'),
-            queue = container.get('queue'),
-            debug = container.get('debug'),
-            profile = container.get('profile');
+  function MessageProxy () {
+    var connectionPool = require('./connection.pool'),
+      queue = container.get('queue'),
+      debug = container.get('debug'),
+      profile = container.get('profile');
 
-        return {
-            forward: forward,
-            bulkForward: bulkForward,
-            getTriggered: getTriggered
-        };
+    return {
+      forward: forward,
+      bulkForward: bulkForward,
+      getTriggered: getTriggered
+    };
 
-        /**
-         * Finds profiles by client properties and sends
-         * the messages or caches them on failure
-         *
-         * @param options
-         * @param message
-         * @param cache
-         */
-        function forward(options, message, cache) {
-            var recipients = connectionPool.filter(options),
-                index = 0;
+    /**
+     * Finds profiles by client properties and sends
+     * the messages or caches them on failure
+     *
+     * @param options
+     * @param message
+     * @param cache
+     */
+    function forward (options, message, cache) {
+      var recipients = connectionPool.filter(options),
+        index = 0;
 
-            // if cache is enabled, check if client exists
-            // and if not, cache the message
-            if(true === cache && 'undefined' !== typeof options.id) {
-                if(recipients.length === 0) {
-                    queue.add(options.id, message);
-                    debug.log('Caching1', JSON.stringify(message));
-                }
-            }
-
-            for (index; index < recipients.length; index++) {
-                var response = recipients[index].sendMessage(message);
-                // it's possible the client was disconnected or
-                // unreachable so this caches the message
-                if(false === response) {
-                    queue.add(options.id, message);
-                    debug.log('Caching2', JSON.stringify(message));
-                }
-            }
+      // if cache is enabled, check if client exists
+      // and if not, cache the message
+      if (true === cache && 'undefined' !== typeof options.id) {
+        if (recipients.length === 0) {
+          queue.add(options.id, message);
+          debug.log('Caching1', JSON.stringify(message));
         }
+      }
 
-        /**
-         * Returns all triggered clients
-         *
-         * @param options
-         * @returns {Object}
-         */
-        function getTriggered(options) {
-            var clients = connectionPool.filter(options),
-                ids = {},
-                index = 0;
-
-            for (index; index < clients.length; index++) {
-                ids[clients[index].id] = profile.get(clients[index].id);
-            }
-
-            return ids;
+      for (index; index < recipients.length; index++) {
+        var response = recipients[index].sendMessage(message);
+        // it's possible the client was disconnected or
+        // unreachable so this caches the message
+        if (false === response) {
+          queue.add(options.id, message);
+          debug.log('Caching2', JSON.stringify(message));
         }
-
-        /**
-         * Forward messages for a list of IDs
-         *
-         * @param ids
-         * @param message
-         * @param cache
-         */
-        function bulkForward(ids, message, cache) {
-            var index = 0;
-
-            for(index; index < ids.length; index++) {
-                forward({id: ids[index]}, message, cache);
-            }
-        }
+      }
     }
 
-    module.exports = new MessageProxy();
+    /**
+     * Returns all triggered clients
+     *
+     * @param options
+     * @returns {Object}
+     */
+    function getTriggered (options) {
+      var clients = connectionPool.filter(options),
+        ids = {},
+        index = 0;
+
+      for (index; index < clients.length; index++) {
+        ids[clients[index].id] = profile.get(clients[index].id);
+      }
+
+      return ids;
+    }
+
+    /**
+     * Forward messages for a list of IDs
+     *
+     * @param ids
+     * @param message
+     * @param cache
+     */
+    function bulkForward (ids, message, cache) {
+      var index = 0;
+
+      for (index; index < ids.length; index++) {
+        forward({id: ids[index]}, message, cache);
+      }
+    }
+  }
+
+  module.exports = new MessageProxy();
 })();
