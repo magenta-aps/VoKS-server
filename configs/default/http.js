e4fc4fef69c9312a03037d9c9b75b895b7635271
Emil Thorenfeldt <emt@magenta-aps.dk>
2019-10-17 13:46:55 +0200

    Make app run HTTP without local files, move defaults

---
+++ configs/default/http.js
@@ -0,0 +1,9 @@
+(function(){
+    var config = {};
+    config.port = 9001;
+
+    config.secure = {};
+    config.secure.enabled = false;
+
+    module.exports = config;
+})();
--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-DEBUG_MODE=${WS_SERVER_DEBUG:-false}
-
-if $DEBUG_MODE; then
-    mv dev-environment/configs/* configs/
-    mv dev-environment/certs certs/
-else
-    rm -rf dev-environment/
-fi
+# Copy default configuration if configuration not already mounted
+cp -n configs/default/* configs/
 
 exec "$@"
