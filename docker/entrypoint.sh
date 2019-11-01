6068fd811ce08666c13b8731ee5d729d60a988ec
Emil Thorenfeldt <emt@magenta-aps.dk>
2019-11-01 13:40:18 +0100

    Add sleep func and check config dir

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 # Copy default configuration if configuration not already mounted
-cp -n configs/default/* configs/
+[ -d configs/default ] && cp -n configs/default/* configs/
+
+if [ ! -z "$WAIT_FOR_SHELTER" ]; then
+    echo "Sleeping for ${WAIT_FOR_SHELTER} seconds"
+    sleep "${WAIT_FOR_SHELTER}"s
+fi
 
 exec "$@"
